75d8cd8bbe3f19faad38598f8cbe4e5b59ac2458
bintangsaktya <muhammad_bintang_27tkj@student.smktelkom-mlg.sch.id>
2022-02-04 12:15:30 +0700

    commitku

---
+++ auth/register.php
@@ -0,0 +1,57 @@
+<?php 
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <!-- CSS -->
+    <link rel="stylesheet" href="../src/css/style.css">
+     <!-- FONTS -->
+     <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;800&display=swap" rel="stylesheet">
+
+</head>
+<body>
+    
+    <section id="content-1-login">
+        
+        <h1>Register</h1>
+           <div class="rounded-box-login">
+               
+                <div class="rounded-box-container-login">
+                    <div class="box-card-login">
+                        <!-- <h3>The fastest way to get sponsorship</h3> -->
+                        <div class="auth">
+                            <form action="" method="post">
+
+                                <h3>Username / Email</h3>
+                                <input type="text" name="username">
+                                
+                                <h3>Password</h3>
+                                <input type="password" name="password"><br>
+
+                                <h3>Confirm Password</h3>
+                                <input type="password" name="password2"><br>
+
+                                <button class="loginButton" type="submit" name="register">Sign Up</button>
+
+                            </form>
+                        </div>   
+                    </div>
+    
+                </div>
+                
+           </div>
+        
+
+    </section>
+
+</body>
+</html>
